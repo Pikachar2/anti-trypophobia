2c4162a47c6cf4d724dc1caa26ec5d20baa1c41d
Pikachar2 <highiq2@hotmail.com>
2024-12-08 12:40:13 -0700

    2.0 logic changes and some of the art assets

--- antitrypophobia/data.lua
+++ antitrypophobia/data.lua
@@ -1,26 +1,23 @@
---[[
 base_dir = "__base__/graphics/entity/"
 mod_dir = "__antitrypophobia__/"
 objects = {"biter-spawner","spitter-spawner","biter-spawner-corpse","spitter-spawner-corpse"}
 
---settings.get_player_settings(player)["Anti_Trypophobia_Enabled"].value
-
---if settings.startup["Anti_Trypophobia_Enabled"].value or nil then
-log ('BEFORE Trypo ------------------')
-	for key, i in ipairs(objects) do
-		if key == 1 or key == 2 then
-			anim = data.raw["unit-spawner"][i].animations
-		elseif key == 3 or key == 4 then
-			anim = data.raw["corpse"][i].animation
-		end
-		
-		for j=1,4 do
-			for k = 1, #anim[j].layers do
-				anim[j].layers[k].filename = anim[j].layers[k].filename:gsub(base_dir,mod_dir)
-				anim[j].layers[k].hr_version.filename = anim[j].layers[k].hr_version.filename:gsub(base_dir,mod_dir)
-			end
+log ('antitrypophobia --- data.raw.corpse: ' .. serpent.block(data.raw["corpse"]["biter-spawner-corpse"]))
+for key, i in ipairs(objects) do
+	if key == 1 or key == 2 then
+		anim = data.raw["unit-spawner"][i].graphics_set.animations
+	elseif key == 3 or key == 4 then
+		anim = data.raw["corpse"][i].animation
+	end
+log ('antitrypophobia --- anim: ' .. serpent.block(anim))
+log ('antitrypophobia --- i: ' .. serpent.block(i))
+--log ('antitrypophobia --- key: ' .. serpent.block(key))
+--log ('antitrypophobia --- data.raw.unit-spawner[biter-spawner]: ' .. serpent.block(data.raw["unit-spawner"][i]))
+log ('antitrypophobia --- data.raw.corpse: ' .. serpent.block(data.raw["corpse"]))
+	
+	for j=1,4 do
+		for k = 1, #anim[j].layers do
+			anim[j].layers[k].filename = anim[j].layers[k].filename:gsub(base_dir,mod_dir)
 		end
 	end
-log ('AFTER Trypo ------------------')
---end
---]]
+end
--- antitrypophobia/settings.lua
+++
@@ -1,8 +0,0 @@
-data:extend({
-  {
-    type = "bool-setting",
-    name = "Anti_Trypophobia_Enabled",
-    setting_type = "runtime-per-user",
-    default_value = true
-  }
-})
